bd0172cd5fb9dd9b7ca78722b9760aa16c9a8728
Premanshu-jha <131666886+Premanshu-jha@users.noreply.github.com>
2024-06-11 14:38:44 +0530

    Employee_manager

---
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        List<Employee> employees = employeeService.findAll();
+
+        model.addAttribute("employees",employees);
+
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String addForm(Model model){
+        Employee employee = new Employee();
+
+        model.addAttribute("employee",employee);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String updateForm(@RequestParam("employeeId") int id,Model model){
+        Employee em = employeeService.findById(id);
+        model.addAttribute("employee",em);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id){
+
+        employeeService.deleteById(id);
+
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/save")
+    public String saveForm(@ModelAttribute("employee") Employee employee){
+
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+}
